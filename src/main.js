50487a8c973aa3933d954f88e7c1891264184969
Victoria Ancalaf <victoria.ancalaf@gmail.com>
2020-08-01 00:54:32 -0400

    pantalla-login

--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+const loginPage = document.getElementById('first-screen');
+const registerPage = document.getElementById('register-screen');
+const registerBtn = document.getElementById('register');
+
+registerBtn.addEventListener('click', () => {
+loginPage.style.display = 'none';
+registerPage.style.display = 'flex';
+});
