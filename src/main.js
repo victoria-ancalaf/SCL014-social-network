e8be41d354541253b98ba7af47e55bc63e349371
Victoria Ancalaf <victoria.ancalaf@gmail.com>
2020-08-04 00:40:00 -0400

    auth-google

--- src/main.js
+++ src/main.js
@@ -12,3 +12,13 @@ registerBtn.addEventListener('click', () => {
   loginPage.style.display = 'none';
   registerPage.style.display = 'flex';
 });
+
+console.log(firebase);
+
+const auth = firebase.auth();
+const googleBtn = document.getElementById("googleBtn");
+
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+googleBtn.onclick = () => auth.signInWithPopup(provider);
