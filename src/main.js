d2ddbe9278e774c038cad2d521c2486bd1925fca
Ane Acevedo <ane.acevedo@gmail.com>
2020-08-12 09:45:14 -0400

    Cambios instalacion firebase en mi repo

--- src/main.js
+++ src/main.js
@@ -1,19 +1,9 @@
 // Este es el punto de entrada de tu aplicacion
-import {
-    register,
-    loginApp,
-    googleLogin,
-} from './lib/index.js';
+
 import {
     routing
 } from './lib/Router.js';
 import { logIn } from './lib/templates/HomeTemplate.js';
-import { signUp } from './lib/templates/RegisterTemplate.js'
-
-
-
-
-
 
 const init = () => {
     document.getElementById('root').appendChild(logIn());
